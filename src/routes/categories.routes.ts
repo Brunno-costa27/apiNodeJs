4766fa5a480c0a44c44969feabf59a79cd8a2662
Brunno Costa <brunno@siasp.com.br>
2021-07-21 22:45:06 -0300

    primeira etapa finalizado

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,30 @@
+import { response, Router } from 'express';
+import { CategoriesRepositories } from '../repositories/categoriesRepositories';
+
+const categoriesRoutes = Router();
+const categoriesRepositories = new CategoriesRepositories();
+
+categoriesRoutes.post('/', (request, response) => {
+
+    const { name, description } = request.body;
+
+    const categoryAlreadyExists = categoriesRepositories.findByName(name);
+
+    if(categoryAlreadyExists){
+        return response.status(400).json({ message: "Category already exists"});
+    }
+
+    categoriesRepositories.create({name, description});
+
+    response.status(201).send();
+
+})
+
+categoriesRoutes.get("/", (request, response) => {
+
+    const all = categoriesRepositories.list();
+
+    response.json(all);
+})
+
+export { categoriesRoutes }
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.json({ message: 'hello world!' });
-});
-
-app.post('/courses', (request, response) => {
-  const { nome } = request.body;
-
-  response.json({ nome });
-});
+app.use("/categories",categoriesRoutes);
 
 app.listen(3333, () => {
   console.log('Rodando!');
